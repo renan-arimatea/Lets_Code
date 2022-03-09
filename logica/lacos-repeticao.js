e99024bc386c025a6072a0753ae5bb6061349870
Renan Arimatea <renanmoraiz@gmail.com>
2022-03-09 19:10:59 -0300

    Trabalhando com Laços de Repetição em Js.

---
+++ logica/lacos-repeticao.js
@@ -0,0 +1,31 @@
+{
+    let count =  1
+        while (count <= 100) {
+            console.log(count)
+        count++
+        }
+
+}
+
+{
+    let count =  1
+
+        do {
+            console.log(count)
+        count++
+
+        } while (count <= 100)
+
+}
+
+{
+    for (let i = 1; i <= 100; i ++) {
+        console.log(i)
+    }
+}
+
+{
+    for (let i = 0; i < 10; i ++) {
+        console.log(i+1);
+    }
+}
